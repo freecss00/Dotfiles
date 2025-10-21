38b40974e7d76fbaab83b11bd59ff0662c9a00aa
freecss <freecss0540@gmail.com>
2025-10-21 14:31:14 +0900

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,30 @@
+#!/bin/zsh
+
+# Define a function which rename a `target` file to `target.backup` if the file
+# exists and if it's a 'real' file, ie not a symlink
+backup() {
+  target=$1
+  if [ -e "$target" ]; then
+    if [ ! -L "$target" ]; then
+      mv "$target" "$target.backup"
+      echo "-----> Moved your old $target config file to $target.backup"
+    fi
+  fi
+}
+
+symlink() {
+  file=$1
+  link=$2
+  if [ ! -e "$link" ]; then
+    echo "-----> Symlinking your new $link"
+    ln -s $file $link
+  fi
+}
+
+for name in vimrc vim zshrc zsh tmux.conf; do
+  if [ ! -d "$name" ]; then
+    target="$HOME/.$name"
+    backup $target
+    symlink $PWD/$name $target
+  fi
+done
